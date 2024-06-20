e212a00c6e28cc18a802df4465900d79f4b3a76e
Tanh <buitanh243@gmail.com>
2024-06-20 19:27:09 +0700

    Đã set quyền đang nhập AD

--- backend/gopy/delete.php
+++ backend/gopy/delete.php
@@ -1,5 +1,11 @@
-<?php session_start() ?>
-<?php 
+<?php session_start(); 
+if (!isset($_SESSION['tk_id']) || $_SESSION['tk_id'] != 1) {
+  echo '<script>
+          location.href="/Last_project/Xuly/popup-login.php?name=Error";
+        </script>';
+  exit; 
+}
+?><?php 
     include_once __DIR__.'/../../connect/connect.php';
 
     $id = $_GET['id'];
